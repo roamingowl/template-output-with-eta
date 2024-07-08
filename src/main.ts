9dd8f303c2410fa170f9cbe53d2adc96a981f4e3
Roaming Owl <161971527+roamingowl@users.noreply.github.com>
2024-07-08 12:19:17 +0200

    refactor(code): extract function to reduce Cognitive Complexity (#43)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import * as core from '@actions/core';
 import { Eta } from 'eta';
-import YAML from 'yaml';
-import dotenv from 'dotenv';
 import * as fs from 'fs';
 import * as dateFns from 'date-fns';
 import { UTCDateMini } from '@date-fns/utc';
+import { parseVariables } from './variables';
 
 /**
  * The main function for the action.
@@ -14,50 +13,22 @@ export async function run(): Promise<void> {
   try {
     let template = core.getInput('template', { required: true });
     const varName = core.getInput('var_name', { required: false }) || 'it';
-    const variables = core.getInput('variables', {
+    const rawVariables = core.getInput('variables', {
       required: false
     });
 
     core.debug(`Template string: ${template}`);
     core.debug(`Variables prefix in template: ${varName}`);
 
-    let parsedVariables: { [key: string]: unknown } | undefined;
+    const parsedVariables = parseVariables(rawVariables);
 
-    if (variables.length > 0) {
-      try {
-        parsedVariables = JSON.parse(variables);
-        core.debug('Variables parsed as JSON');
-      } catch {
-        // If the JSON parse fails, try to parse as YAML
-      }
-
-      if (typeof parsedVariables !== 'object') {
-        try {
-          parsedVariables = YAML.parse(variables);
-          core.debug('Variables parsed as YAML');
-        } catch {
-          // If the YAML parse fails, try parse detoenv
-        }
-      }
-
-      if (typeof parsedVariables !== 'object') {
-        parsedVariables = dotenv.parse(Buffer.from(variables, 'utf8'));
-        if (typeof parsedVariables === 'object' && Object.keys(parsedVariables).length === 0) {
-          core.debug('No variables found by dotenv');
-          parsedVariables = undefined;
-        }
-      }
-
-      if (typeof parsedVariables !== 'object') {
-        core.error('Unable ot parse variables as JSON or YAML');
-      }
-    }
     const eta = new Eta({
       varName,
       functionHeader: 'const utils = it._utilsInternal; const UTCDateMini = utils.UTCDateMini;'
     });
 
     if (fs.existsSync(template)) {
+      core.debug('Template is a file, reading it...');
       template = fs.readFileSync(template, 'utf8');
     }
 
